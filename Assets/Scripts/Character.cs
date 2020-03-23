0f6d10b022e5e01b72a8c193bbdeea208ef4b6dc
Jrnoel <jonathan.roynoel@gmail.com>
2020-03-23 19:30:46 -0400

    Character stats

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+	[Header("Stats")]
+	[SerializeField]
+	private CharacterStat maxHealthPoints = null;
+
 	[SerializeField]
 	private bool canBeTargeted = true;
 
+	public int HealthPoints { get; private set; }
 	public bool CanBeTargeted => canBeTargeted;
 	public bool HasTarget => Target != null;
 
 	public Character Target { get; private set; }
 
 	public event Action<Character> OnTargetSet;
+
+	private void Start()
+	{
+		HealthPoints = (int)maxHealthPoints.Value;
+	}
 
 	public void SetTarget(Character newTarget)
 	{
@@ -21,5 +31,31 @@ public class Character : MonoBehaviour
 			print($"{gameObject.name} is now targeting {newTarget.name}");
 			OnTargetSet?.Invoke(Target);
 		}
+	}
+
+	public void TakeDamage(int amount)
+	{
+		HealthPoints -= amount;
+		print($"{gameObject.name} took {amount} damage.");
+		if (HealthPoints <= 0)
+		{
+			Die();
+		}
+	}
+
+	public void Heal(int amount)
+	{
+		HealthPoints += amount;
+		print($"{gameObject.name} healed for {amount}.");
+		if (HealthPoints > (int)maxHealthPoints.Value)
+		{
+			HealthPoints = (int)maxHealthPoints.Value;
+		}
+	}
+
+	private void Die()
+	{
+		Destroy(gameObject);
+		print(gameObject.name + " died!");
 	}
 }
